4f816f31890d73496fb0f2238ed11caac15618fe
Ken Sodemann <ken.sodemann@gmail.com>
2017-08-16 15:51:09 -0500

    log out the lifecycle callbacks

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,9 +6,33 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  page = 'ContactPage';
 
-  constructor(public navCtrl: NavController) {
+  constructor(private navCtrl: NavController) { }
 
+  ionViewCanEnter(): boolean {
+    console.log(`${this.page} can enter`);
+    return true;
   }
 
+  ionViewCanLeave(): boolean {
+    console.log(`${this.page} can leave`);
+    return true;
+  }
+
+  ionViewWillEnter() {
+    console.log(`${this.page} will enter`);
+  }
+
+  ionViewDidEnter() {
+    console.log(`${this.page} did enter`);
+  }
+
+  ionViewWillLeave() {
+    console.log(`${this.page} will leave`);
+  }
+
+  ionViewDidLeave() {
+    console.log(`${this.page} did leave`);
+  }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,33 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  page = 'HomePage';
 
-  constructor(public navCtrl: NavController) {
+  constructor(private navCtrl: NavController) { }
 
+  ionViewCanEnter(): boolean {
+    console.log(`${this.page} can enter`);
+    return true;
   }
 
+  ionViewCanLeave(): boolean {
+    console.log(`${this.page} can leave`);
+    return true;
+  }
+
+  ionViewWillEnter() {
+    console.log(`${this.page} will enter`);
+  }
+
+  ionViewDidEnter() {
+    console.log(`${this.page} did enter`);
+  }
+
+  ionViewWillLeave() {
+    console.log(`${this.page} will leave`);
+  }
+
+  ionViewDidLeave() {
+    console.log(`${this.page} did leave`);
+  }
 }
