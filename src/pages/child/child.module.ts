74677e7f4796c4ce7992e320a79225e4fa09a680
Ken Sodemann <ken.sodemann@gmail.com>
2017-08-17 17:44:42 -0500

    feat(lazyLoad): ipmlement lazy loading

---
+++ src/pages/child/child.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ChildPage } from './child';
+
+@NgModule({
+  declarations: [ChildPage],
+  imports: [IonicPageModule.forChild(ChildPage)],
+  entryComponents: [ChildPage]
+})
+export class ChildPageModule { }
