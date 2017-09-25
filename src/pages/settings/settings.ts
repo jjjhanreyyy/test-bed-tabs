e9a606bc9ca11e6c86b508a44e0c51131f95e7cc
Ken Sodemann <ken@ionic.io>
2017-09-25 16:02:01 -0500

    iOS 11 updates

---
+++ src/pages/settings/settings.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-settings',
+  templateUrl: 'settings.html',
+})
+export class SettingsPage {
+
+  constructor() {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SettingsPage');
+  }
+
+}
